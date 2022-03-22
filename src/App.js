d1f99c2026c5b1f9da1c90f1ea4f9f2d8c6b1b7d
kushalsourav <kushalsourav2@gmail.com>
2022-03-22 13:47:33 +0530

    created  Router component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import { Route, Routes } from "react-router-dom";
+import Router  from './Components/Router/Router';
 import "./App.css";
-import Home from "./Components/Home/Home";
-import Navbar from "./Components/navbar/Navbar";
-import Products from "./Components/Products/Products";
-import Cart from "./Components/Cart/Cart";
-import Wishlist from "./Components/Wishlist/Wishlist";
-import Login from "./Components/Form/Login";
-
 
 function App() {
   return (
     <div>
-     <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Products" element={<Products />} />
-        <Route path="/Cart" element={<Cart />} />
-        <Route path="/Wishlist" element={<Wishlist />} />
-        <Route path="/Login" element={<Login />} />
-      </Routes>
+     <Router />
     </div>
 
   );
---
+++ src/Components/Router/Router.js
@@ -0,0 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../Home/Home";
+import Products from "../Products/Products";
+import Cart from "../Cart/Cart";
+import Wishlist from "../Wishlist/Wishlist";
+import Login from "../Form/Login";
+import Navbar from "../navbar/Navbar";
+import PageNotFound from "../PageNotFound/PageNotFound";
+const Router = () => {
+    
+    return (
+        <>
+        <Navbar />
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Products" element={<Products />} />
+        <Route path="/Cart" element={<Cart />} />
+        <Route path="/Wishlist" element={<Wishlist />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        </>
+    )
+
+}
+
+export default Router;
